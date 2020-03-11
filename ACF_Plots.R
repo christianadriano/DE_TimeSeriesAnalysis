3a89dee7684d4c35ba2340fb48e95794482bf063
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-03-11 19:53:07 +0100

    Update ACF_Plots.R
    wrote to file

--- ACF_Plots.R
+++ ACF_Plots.R
@@ -6,22 +6,14 @@ of significance threshold.
 
 library(stringr)
 library(tseries)
+install.packages("Rmisc")
+library(Rmisc)
 
 #------------------------------
 #LOAD DATA
 root <-  "C://Users//Christian//Documents//GitHub//DE_TimeSeriesAnalysis//data//"
 file_name = "avg_runtimes"
 dt.set <- read.csv(str_c(root,file_name,".csv"))
-
-#-------------------------------
-
-#Plot time series
-
-#wrangle the dataset
-y= dt.set$Frankfurt_b[!is.na(dt.set$Frankfurt_b)]
-x=c(1:length(y))
-df = data.frame(x,y)
-
 
 #--------------------------------
 
@@ -38,51 +30,28 @@ add_dataframe <- function(df_results, row, threshold, series.size,
 }
 
 
-#-------------------------------
-acf_model <- function(y, series.name){
-  
-  "ACF model"
-  lag.length = length(y)
-  acf_data <- acf(y,lag.max = lag.length,
-                  xlab = "lag #", ylab = str_c('ACF ',series.name),
-                  main=series.name, 
-                  plot = FALSE)
-  
-  #Plot without the first correlation value (which is One by default)
-  plot(acf_data[2:length(acf_data$acf)])
-  
-  return(acf_data);
-}
 
 #-------------------------------
-"Partial Auto-correlation model"
-pacf_model <- function(df_results, series, series.name){
-  
-  data <- pacf(series,
-              lag.max = length(series),
-              xlab = "lag #",
-              ylab = str_c('PACF ',series.name),
-              main=series.name,
-              plot = FALSE)
+"Plot ACF model and find the highest non-significant lag"
+find_threshold <- function(df_results, data, index, model.name, series.name){
   
   #plot without first data point because it is always 1 and
   #it distorts the plot visualization
-  plot(data[2:length(data$acf)])
+  plot(data[2:length(data$acf)], main=str_c(model.name,".",series.name))
   
   #Discover the lag after which all values are significant
   #remove all significant points.
-  df <- data.frame(lag=data$lag,pacf=data$acf)
-  df <- df[df$pacf>0.025 | df$pacf< -0.025,]
+  df <- data.frame(lag=data$lag,acf=data$acf)
+  df <- df[df$acf>0.025 | df$acf< -0.025,]
   threshold <-  max(df$lag)
   
   df_results <- add_dataframe(df_results,index,threshold, 
-                              length(series), model.name,series.name)
+                              length(data$lag), model.name, series.name)
   
   return (df_results)
-  
 }
 
-plot(acf_data[acf_data<0.025 & acf_data>-0.025])
+#plot(acf_data[acf_data<0.025 & acf_data>-0.025])
 
 #------------------------------
 #MAIN
@@ -90,8 +59,8 @@ plot(acf_data[acf_data<0.025 & acf_data>-0.025])
 series.names.list = colnames(dt.set)
 
 #create the data.frame for all test statitics
-#3 times because three tests per series.name
-col.length = length(series.names.list) * 3;
+#2 times because two ACF models per series.name
+col.length = length(series.names.list) * 2;
 
 df_results <- data.frame(
   series.name=character(col.length),
@@ -101,18 +70,56 @@ df_results <- data.frame(
   stringsAsFactors =FALSE
 )
 
-index = 1
-i=1
+plot_array <-  vector('list',length=2)
+
+index <-  1
+#i <- 1
+
 for(i in c(1:length(series.names.list))){
   series <- dt.set[,i]
   series <- series[!is.na(series)]
-  df_results <- pacf_model(series,index,series.names.list[i])
-  index = index + 3
+  series.name <- series.names.list[i]
+  
+  #ACF
+  model.name = "ACF"
+  data <- acf(series,lag.max = length(series),
+               xlab = "lag #",
+               ylab = str_c(model.name),
+               main=series.name, plot = FALSE)
+  
+
+  # plot_array[[index]] <- local({   
+  #                           p <- plot(data[2:length(data$acf)], 
+  #                                            main=str_c(model.name,".",series.name))
+  #                           print(p)
+  #                       })
+  
+  df_results <- find_threshold(df_results, data,index, 
+                               model.name, series.name)
+  index = index+1
+ 
+  #PACF
+  model.name = "PACF"
+  data <- pacf(series, lag.max = length(series),
+               xlab = "lag #",
+               ylab = str_c(model.name),
+               main=series.name, plot = FALSE)
+  
+  # plot_array[[index]]  <- plot(data[2:length(data$acf)], 
+  #                                   main=str_c(model.name,".",series.name))
+  #                             print(p)
+  # })
+  
+  df_results <- find_threshold(df_results, data,index, 
+                               model.name, series.name)
+  
+  index = index + 1
 }
 
+#multiplot(plot_array[1],plot_array[2],2)
 
 #WRITE TO FILE
-csv_file <- str_c(root,"stationarity_test_results",".csv")
+csv_file <- str_c(root,"threshold_results",".csv")
 write.table(df_tests, file = csv_file, sep = ",", 
             col.names = !file.exists(csv_file), 
             row.names = FALSE,
